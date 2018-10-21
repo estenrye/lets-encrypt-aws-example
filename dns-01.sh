d9f0bb71681508800d9a67c978ccafe40c259ec6
Esten Rye <esten@ryezone.net>
2018-10-20 20:33:06 -0500

    DNS-01 challenge working.

--- dns-01.sh
+++ dns-01.sh
@@ -2,7 +2,7 @@ mkdir -p /opt/traefik
 touch /opt/traefik/acme.json
 chmod 600 /opt/traefik/acme.json
 if [ -z $(docker network ls --filter name=public --format {{.ID}}) ]; then
-  docker network create public --driver overlay && docker stack deploy -c http-01.yml http01
+  docker network create public --driver overlay && docker stack deploy -c dns-01.yml dns01
 else
-  docker stack deploy -c http-01.yml http01
+  docker stack deploy -c dns-01.yml dns01
 fi
